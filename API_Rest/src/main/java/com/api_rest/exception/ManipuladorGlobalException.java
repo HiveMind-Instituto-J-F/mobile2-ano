f1df03f011f17ff2a04efce308fc40ac71c91fe2
Liberato <157623242+Liberatinho@users.noreply.github.com>
2025-10-14 08:06:45 -0300

    Alterando API_Rest para um reposítorio próprio

--- API_Rest/src/main/java/com/api_rest/exception/ManipuladorGlobalException.java
+++
@@ -1,9 +0,0 @@
-package com.api_rest.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ManipuladorGlobalException {
-
-
-}
--- API_Rest/src/main/java/com/api_rest/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api_rest.repository;
-
-import com.api_rest.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
